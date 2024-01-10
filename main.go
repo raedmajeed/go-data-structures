c21080baf30054f6217386969be58bb78cba1474
Raed Abdul Majeed <100485450+raedmajeed@users.noreply.github.com>
2024-01-10 08:52:36 +0530

    added basic tree operations

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import pkg "github.com/raedmajeed/data-structures/pkg/binary_search_trees"
 
+type ss struct {
+	age int
+}
+
+func main() {
+	pkg.TreeOperations()
+	//s := []ss{
+	//	{age: 1},
+	//	{age: 1},
+	//}
 }
---
+++ pkg/binary_search_trees/basic_operations.go
@@ -0,0 +1,59 @@
+package binary_search_trees
+
+import "fmt"
+
+type TreeNode struct {
+	Data  int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type Tree struct {
+	Node *TreeNode
+}
+
+func (t *Tree) Insert(value int) {
+	t.Node = RecInsert(t.Node, value)
+}
+
+func RecInsert(root *TreeNode, value int) *TreeNode {
+	if root == nil {
+		newRoot := &TreeNode{Data: value}
+		return newRoot
+	}
+
+	if value < root.Data {
+		root.Left = RecInsert(root.Left, value)
+	} else if value > root.Data {
+		root.Right = RecInsert(root.Right, value)
+	}
+	return root
+}
+
+func (t *Tree) BFS() {
+	queue := []*TreeNode{t.Node}
+
+	for len(queue) != 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		data := curr.Data
+		fmt.Println(data, " -> ")
+
+		if curr.Left != nil {
+			queue = append(queue, curr.Left)
+		}
+		if curr.Right != nil {
+			queue = append(queue, curr.Right)
+		}
+	}
+}
+
+func TreeOperations() {
+	tree := &Tree{}
+	tree.Insert(5)
+	tree.Insert((6))
+	tree.Insert((3))
+	tree.Insert((7))
+
+	tree.BFS()
+}
